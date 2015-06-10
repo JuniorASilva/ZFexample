56a603669f81e60f744657a6ea07b4ec0523b17b
Junior Aldair Silva Portocarrero <jsilvap22@gmail.com>
2015-06-10 14:15:21 -0500

    Adecion de un modulo fuera

--- pruebaZ/config/application.config.php
+++ pruebaZ/config/application.config.php
@@ -14,11 +14,13 @@ return array(
         'Moel',
         'Usuario',
         'Cron',
+        'Prueba',
     ),
     'module_listener_options' => array(
         'module_paths' => array(
             './module',
             './vendor',
+            './website',
         ),
         'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
